362ffe00c54f792723dc19315b915521ef58690e
RAMAHALISON Antoniaina <arisehenoantoniaina@gmail.com>
2025-08-22 23:51:03 +0300

    feat(render): add basic ASCII rendering engine with buffer

--- src/main.c
+++ src/main.c
@@ -1,35 +1,19 @@
-#include<stdio.h>
-#include<Windows.h>
-
-#define HEIGHT_MAX 20
+#include <stdio.h>
+#include <Windows.h>
+#include "render.h"
 
 int main() {
-    double y = 18.0;
-    double vy = 0.0;
-    double g = 9.81;
-    double dt = 0.05;
-    double el = 0.8;
-    double t = 0.0;
-    
+    char screen[HEIGHT][WIDTH];
+
+    int x=5, y=5 ;
+
     while (1){
-        vy -= g * dt;
-        y += vy * dt;
-
-        if ( y <= 0 ) {
-            y = 0; // TODO: consider the speed at the collision point
-            vy = -vy*el;
-        }
-
-        int pos = (int)(HEIGHT_MAX - y + 0.5);
-        system("cls");
-        printf("t=%.2f (s)  |   y=%.2f (m)  |    vy=%.2f (m/s)   \n", t, y, vy);
-        printf("-------------------------------------------------\n");
-        for (int i=0 ; i<HEIGHT_MAX ; i++) {
-            printf(i == pos ? "    O\n" : "\n" );
-        }        
-        printf("-------------------------------------------------");
-        t += dt;
-        Sleep(30);
+       initBuffer(screen);
+       drawChar(screen, x, y, 'O');
+       drawBuffer(screen);
+       x = (x+1) % WIDTH;
+       Sleep(100);
     }
     
+    return 0;
 }
